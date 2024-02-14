f71308e72ee58c66a96228e766c8e5a588710f66
GabryelKadmo <gabryelkadmo@gmail.com>
2024-02-14 19:49:32 -0300

    fix: melhorias na landing page

--- src/App/Pages/LandingPage/LandingPage.tsx
+++ src/App/Pages/LandingPage/LandingPage.tsx
@@ -2,6 +2,8 @@ import "./LandingPage.css";
 import introLandingPage from "../../../assets/images/LandingPage/IntroLandingPage.png";
 import JackDanielsImage from "../../../assets/images/LandingPage/JackDaniels.png";
 import RedLabelImage from "../../../assets/images/LandingPage/RedLabel.png";
+import WoodfordReserveImage from "../../../assets/images/LandingPage/WoodfordReserve.png";
+import OldParrImage from "../../../assets/images/LandingPage/OldParr.png";
 
 export function LandingPage() {
   return (
@@ -29,21 +31,39 @@ export function LandingPage() {
       </div>
       <div className="WhiskiesInfo">
         <div className="WhiskiesBG">
-          <div className="WhiskyFocus">
-            <img className="jackDanielsImg" src={JackDanielsImage} alt="" />
-          </div>
-          <p className="textJackDaniels">
-            O Jack Daniel's é uma escolha clássica, oferecendo um sabor
-            <br></br>suave e marcante com notas de baunilha e carvalho tostado.
-            <br></br> Ideal para apreciadores de whisky.
+          <img className="jackDanielsImg" src={JackDanielsImage} alt="" />
+          <p className="DescJackDaniels">
+            O Jack Daniel's é uma escolha clássica, oferecendo um sabor suave e
+            marcante com notas de baunilha e carvalho tostado. Ideal para
+            apreciadores de whisky.
           </p>
         </div>
         <div className="WhiskiesBG">
           <img className="RedLabelImg" src={RedLabelImage} alt="" />
-          <p className="textJackDaniels">
+
+          <p className="DescRedLabel">
             O Johnnie Walker Red Label é uma escolha clássica e acessível,
-            <br></br>
             perfeita para desfrutar em qualquer ocasião.
+          </p>
+        </div>
+        <div className="WhiskiesBG">
+          <img
+            className="WoodfordReserveImg"
+            src={WoodfordReserveImage}
+            alt=""
+          />
+          <p className="DescWoodfordReserve">
+            O Whisky Woodford Reserve é sofisticado e distinto, com notas de
+            carvalho tostado, baunilha e especiarias. Ideal para apreciadores de
+            bourbon.
+          </p>
+        </div>
+        <div className="WhiskiesBG">
+          <img className="OldParrImg" src={OldParrImage} alt="" />
+          <p className="DescOldParr">
+            O Whisky Old Parr 12 anos oferece um sabor refinado e encorpado, com
+            notas suaves de frutas secas, mel e especiarias. Ideal para
+            apreciadores de whisky.
           </p>
         </div>
       </div>
