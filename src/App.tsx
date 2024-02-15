423c7be108adbd19ae06697d1a3156474b6c6d4e
GabryelKadmo <gabryelkadmo@gmail.com>
2024-02-15 17:28:51 -0300

    feat: sessão de marcas famosas na landing page

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@
 import { Header } from "./App/Components/Header/Header";
 import {LandingPage} from "./App/Pages/LandingPage/LandingPage";
 import {Footer} from "./App/Components/Footer/Footer";
+import {Brands} from "./App/Components/BrandSection/Brands";
 function App() {
 
   return (
     <>
       <Header/>
       <LandingPage />
+      <Brands/>
       <Footer/>
       </>
   );
---
+++ src/App/Components/BrandSection/Brands.tsx
@@ -0,0 +1,10 @@
+import "./Brands.css";
+// import img from "../../../assets/images/LandingPage/IntroLandingPage.png"
+
+export function Brands() {
+    return (
+        <section className="BrandSection">
+                {/* <img src={img} alt="" /> */}
+        </section>
+    )
+}
