2cc2bf025c7d7bbb2a73d5a9f5350edd6d2d7dc2
GabryelKadmo <gabryelkadmo@gmail.com>
2024-02-15 17:30:06 -0300

    Fix: correção do commit anterior

--- src/App/Components/BrandSection/Brands.tsx
+++ src/App/Components/BrandSection/Brands.tsx
@@ -1,10 +1,26 @@
 import "./Brands.css";
-// import img from "../../../assets/images/LandingPage/IntroLandingPage.png"
+import JackDanielsLogo from "../../../assets/images/BrandsImgs/JackDanielsLogo.png"
+import DalmoreLogo from "../../../assets/images/BrandsImgs/DalmoreLogo.png"
+import OldParrLogo from "../../../assets/images/BrandsImgs/OldParrLogo.png"
+import WoodfordReserveLogo from "../../../assets/images/BrandsImgs/WoodfordReserveLogo.png"
+import JohnnieWalkerLogo from "../../../assets/images/BrandsImgs/JohnnieWalkerLogo.png"
+import ChivasRegalLogo from "../../../assets/images/BrandsImgs/ChivasRegalLogo.png"
+import BallantineLogo from "../../../assets/images/BrandsImgs/BallantineLogo.png"
+
 
 export function Brands() {
     return (
         <section className="BrandSection">
-                {/* <img src={img} alt="" /> */}
+            <h1 className="titleBrandSection">MARCAS FAMOSAS</h1>
+            <div>
+                <img className="imgBrandsArea" src={JackDanielsLogo} alt="" />
+                <img className="imgBrandsArea" src={DalmoreLogo} alt="" />
+                <img className="imgBrandsArea" src={OldParrLogo} alt="" />
+                <img className="imgBrandsArea" src={WoodfordReserveLogo} alt="" />
+                <img className="imgBrandsArea" src={JohnnieWalkerLogo} alt="" />
+                <img className="imgBrandsArea" src={ChivasRegalLogo} alt="" />
+                <img className="imgBrandsArea" src={BallantineLogo} alt="" />
+            </div>
         </section>
     )
 }
