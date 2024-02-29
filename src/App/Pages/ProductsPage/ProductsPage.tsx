9b3da3bb35fcafc520f8d1d8dcdc7e28e870e4dd
GabryelKadmo <gabryelkadmo@gmail.com>
2024-02-29 01:24:53 -0300

    feat: tela de produtos adicionada

---
+++ src/App/Pages/ProductsPage/ProductsPage.tsx
@@ -0,0 +1,9 @@
+import "./ProductsPage.css"
+
+export default function ProductsPage() {
+    return (
+        <div className="ProductsPage">
+            <h1>ProductsPage</h1>
+        </div>
+    )
+}
